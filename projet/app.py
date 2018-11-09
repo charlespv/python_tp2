ce4a87d05cee4cf7f5fd71329418d0462d5803ef
charlespv <charles.paulas-victor@hetic.net>
2018-11-09 16:12:36 +0100

    update render_selec()

--- projet/app.py
+++ projet/app.py
@@ -3,7 +3,7 @@ import form.render as f
 if __name__ == "__main__":
     select = f.render_select()
     button = f.render_button()
-    s = f.render_form(select+button)
+    s = f.render_form(select + button)
     print(s)
 
 
--- projet/form/render.py
+++ projet/form/render.py
@@ -1,26 +1,36 @@
 def render_select():
-    # Display static example of a html select tag    
-    s = '<select name="couleurs\">\n\
-            \t<option value="#ff9900" />orange</option>\n\
-            \t<option value="#00ff00" />vert</option>\n\
-            \t<option value="#ff0000" selected />rouge</option> \n\
-            \t<option value="#ff00ff" />violet</option>\n\
-            \t<option value="#0000ff" />bleu</option>\n\
-            \t<option value="#000000" />noir</option>\n\
-            \t<option value="#ffffff" />blanc</option>\n\
-            \t<option value="#ffff00" />jaune</option>\n\
-        </select>\n\t'
-    return s
+    # Display static example of a html select tag 
+    couleurs = {
+        '#ff9900': 'orange',
+        '#00ff00': 'vert',
+        '#ff0000': 'rouge',
+        '#ff00ff': 'violet',
+        '#0000ff': 'bleu',
+        '#000000': 'noir',
+        '#ffffff': 'blanc',
+        '#ffff00': 'jaune'
+    }
+    s=''
+    first = '\t<select name="couleurs">\n'
+    for cnt in couleurs:
+        s += ('\t\t<option value="{}" {} />{}</option>\n'.
+                format(cnt, 
+                    "selected" if couleurs[cnt] == 'rouge' else "", 
+                    couleurs[cnt]))
+    second = '\t</select>\n'
+
+    return first + s + second
+
 
 def render_button():
     # Display static example of a html input used as button   
-    s = '<input type="submit" value="Envoyer">\n'
+    s = '\t<input type="submit" value="Envoyer">\n'
     return s
 
 def render_form(code):
     # code : str : 
     # Display static example of a html form tag   
-    first_part = '<form>\n\t '
+    first_part = '<form>\n'
     second_part = '</form>'
     s = first_part + str(code) + second_part
     return str(s)
